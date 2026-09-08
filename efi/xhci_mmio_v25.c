ef150cdd141d4f4e6408354602a562f0b16d5e0b
matjp <matthew.penwill@gmail.com>
2026-09-08 13:09:57 +1200

    Make V25 test post-reset DCBAAP initialization

--- efi/xhci_mmio_v25.c
+++ efi/xhci_mmio_v25.c
@@ -20,7 +20,7 @@ static EFI_STATUS mmio_write32(EFI_PCI_IO_PROTOCOL *p, UINT32 off, UINT32 v) {
     return uefi_call_wrapper(p->Mem.Write,6,p,EfiPciIoWidthUint32,0,(UINT64)off,1,&v);
 }
 static void done(UINT32 reads, EFI_STATUS s) {
-    Print(u"\r\nV25 COMPLETE / %u MMIO READS / 2 CONTROL WRITES / NO DMA\r\n",reads);
+    Print(u"\r\nV25 COMPLETE / %u MMIO READS / 3 CONTROL WRITES / NO DMA\r\n",reads);
     Print(u"RESULT: %r\r\nEXIT 5 SEC...\r\n",s);
     uefi_call_wrapper(BS->Stall,1,5000000);
 }
@@ -32,7 +32,8 @@ EFI_STATUS efi_main(EFI_HANDLE image,EFI_SYSTEM_TABLE *st) {
     UINTN n=0,i;
     UINT32 cls=0,id=0,bar0=0,bar1=0,cap0=0;
     UINT32 opbase,cmd=0,status=0,after=0,after_status=0,reads=0;
-    UINT64 bar;
+    UINT64 bar,dcbaap=0,dcbaap_read=0;
+    EFI_PHYSICAL_ADDRESS page=0;
 
     InitializeLib(image,st);
     Print(u"TOSHIBA xHCI V25 / HALT THEN HOST CONTROLLER RESET\r\n");
@@ -144,9 +145,48 @@ EFI_STATUS efi_main(EFI_HANDLE image,EFI_SYSTEM_TABLE *st) {
         Print(u"POSTCONDITION FAIL: CONTROLLER NOT READY\r\n");
         result=EFI_TIMEOUT;
     } else {
-        Print(u"HALT THEN HOST CONTROLLER RESET: PASS\r\n");
-        Print(u"POST-RESET STATE REQUIRES REINITIALIZATION\r\n");
-        Print(u"V25: REINITIALIZATION NOT YET PERFORMED\r\n");
+        Print(u"POST-RESET STATE VERIFIED: HALTED AND READY\r\n");
+        Print(u"V25: PROGRAM DCBAAP WHILE HALTED / NO DMA\r\n");
+
+        s=uefi_call_wrapper(BS->AllocatePages,4,AllocateAnyPages,
+                            EfiBootServicesData,1,&page);
+        if(EFI_ERROR(s)){
+            Print(u"DCBAA ALLOC FAIL %r\r\n",s);
+            result=s;
+        } else {
+            uefi_call_wrapper(BS->SetMem,3,(void*)(UINTN)page,4096,0);
+            dcbaap=(UINT64)page;
+
+            if(dcbaap & 0x3fULL){
+                Print(u"DCBAA ALIGN FAIL %016lx\r\n",dcbaap);
+                result=EFI_BAD_BUFFER_SIZE;
+            } else {
+                Print(u"DCBAAP WRITE %016lx\r\n",dcbaap);
+                s=uefi_call_wrapper(p->Mem.Write,6,p,EfiPciIoWidthUint64,0,
+                                    (UINT64)(opbase+0x30),1,&dcbaap);
+                if(EFI_ERROR(s)){
+                    Print(u"DCBAAP WRITE FAIL %r\r\n",s);
+                    result=s;
+                } else {
+                    s=uefi_call_wrapper(p->Mem.Read,6,p,EfiPciIoWidthUint64,0,
+                                         (UINT64)(opbase+0x30),1,&dcbaap_read);
+                    reads++;
+                    if(EFI_ERROR(s)){
+                        result=s;
+                    } else {
+                        reads++;
+                        Print(u"DCBAAP READBACK %016lx\r\n",dcbaap_read);
+                        if(dcbaap_read!=dcbaap){
+                            Print(u"DCBAAP READBACK FAIL\r\n");
+                            result=EFI_DEVICE_ERROR;
+                        } else {
+                            Print(u"DCBAAP PROGRAM: PASS / RUN=0 / NO DMA\r\n");
+                        }
+                    }
+                }
+            }
+            uefi_call_wrapper(BS->FreePages,2,page,1);
+        }
     }
 
     if(hs)FreePool(hs);
