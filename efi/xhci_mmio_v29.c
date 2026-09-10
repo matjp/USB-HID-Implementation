ecf2fb8f23f5eb697f3b6d852083e21b0f9f7a6e
matjp <matthew.penwill@gmail.com>
2026-09-10 15:33:49 +1200

    V29: restore write count in provenance test

--- efi/xhci_mmio_v29.c
+++ efi/xhci_mmio_v29.c
@@ -335,6 +335,7 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *st) {
     writes++;
     s=mmio_write64_split(p,rtsoff+0x20+0x10,erst_dev);
     if(EFI_ERROR(s)) goto teardown;
+    writes++;
 
     s=mmio64_split(p,rtsoff+0x20+0x10,&erstba_rd);
     if(EFI_ERROR(s)) { Print(L"READBACK ERSTBA FAIL\\r\\n"); goto teardown; }
